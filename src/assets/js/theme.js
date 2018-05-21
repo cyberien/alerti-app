fe92a7f0058283d048dc45493a61b6d06cf1d656
Evgenii S <simzikov@gmail.com>
2018-05-22 00:38:00 +0300

    added new spacing size, added tooltips to avatars

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -244,3 +244,30 @@ var Navbar = (function() {
   })
   
 })();
+
+
+/*** Tooltip ***/
+
+var Tooltip = (function() {
+
+  // Variables
+  // =========
+
+  var $tooltip = $('[data-toggle="tooltip"]');
+
+  // Methods
+  // =======
+
+  function init() {
+    $tooltip.tooltip();
+  }
+
+
+  // Events
+  // ======
+
+  if( $tooltip.length ) {
+    init();
+  }
+  
+})();
