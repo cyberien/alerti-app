5b65083f5fd2e32edde9792a216a94901286f42e
yevgenysim <yevgenysim@gmail.com>
2020-07-26 11:45:34 +0400

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,34 @@
-const CopyPlugin = require('copy-webpack-plugin');
-const HandlebarsPlugin = require('handlebars-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const WebpackOnBuildPlugin = require('on-build-webpack');
-const fs = require('fs');
-const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const HandlebarsPlugin = require("handlebars-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: {
-    'libs': './src/scss/libs.scss',
-    'theme': ['./src/js/theme.js', './src/scss/theme.scss'],
+    libs: "./src/scss/libs.scss",
+    theme: ["./src/js/theme.js", "./src/scss/theme.scss"],
   },
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, 'src/scss'),
+        include: path.resolve(__dirname, "src/scss"),
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               url: false,
             },
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
           },
         ],
       },
@@ -46,7 +44,7 @@ module.exports = {
         },
         cssProcessorPluginOptions: {
           preset: [
-            'default',
+            "default",
             {
               discardComments: {
                 removeAll: true,
@@ -66,36 +64,34 @@ module.exports = {
     ],
   },
   output: {
-    filename: './assets/js/[name].bundle.js',
+    filename: "./assets/js/[name].bundle.js",
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: './src/favicon',
-          to: './assets/favicon',
-        }
-        {
-          from: './src/fonts',
-          to: './assets/fonts',
+          from: "./src/favicon",
+          to: "./assets/favicon",
         },
         {
-          from: './src/img',
-          to: './assets/img',
+          from: "./src/fonts",
+          to: "./assets/fonts",
+        },
+        {
+          from: "./src/img",
+          to: "./assets/img",
         },
       ],
     }),
     new HandlebarsPlugin({
-      entry: path.join(process.cwd(), 'src', 'html', '**', '*.html'),
-      output: path.join(process.cwd(), 'dist', '[path]', '[name].html'),
-      partials: [path.join(process.cwd(), 'src', 'partials', '**', '*.{html,svg}')],
+      entry: path.join(process.cwd(), "src", "html", "**", "*.html"),
+      output: path.join(process.cwd(), "dist", "[path]", "[name].html"),
+      partials: [
+        path.join(process.cwd(), "src", "partials", "**", "*.{html,svg}"),
+      ],
     }),
     new MiniCssExtractPlugin({
-      filename: './assets/css/[name].bundle.css',
-    }),
-    new WebpackOnBuildPlugin(function () {
-      fs.unlinkSync(path.join('./dist/assets/js', 'libs.bundle.js'));
-      fs.unlinkSync(path.join('./dist/assets/js', 'libs.bundle.js.map'));
+      filename: "./assets/css/[name].bundle.css",
     }),
   ],
 };
